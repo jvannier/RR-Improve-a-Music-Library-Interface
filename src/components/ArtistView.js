883dd7e3a39120a6ab24ec517d75b20a3d511188
Jennifer Vannier <Jen@Jennifers-MacBook-Pro.local>
2022-02-04 23:38:59 -0500

    solution to Code_Along (7.3.9)

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,18 +1,59 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+
 
 const ArtistView = () => {
-    const { id } = useParams()
+    const { artist } = useParams()
     const [ artistData, setArtistData ] = useState([])
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const API_URL = `https://itunes.apple.com/lookup?id=111051&entity=song`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [artist])
+
+    let seen_albums = []
+    let add = false
+    const justAlbums = artistData.filter(entry => {
+        add = !(entry.collection in seen_albums)
+        seen_albums.push(entry.collectionId)
+        return add
+    })
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <Link to={`/album/${artist}/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            <h2>The id passed was: {artist}</h2>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderAlbums}
         </div>
     )
 }
 
 export default ArtistView
+
